418b114ff3f35a17730d1241d7d934c899334dcc
Will Parker <wprk14@gmail.com>
2013-04-15 15:08:08 +0100

    Template has pages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+$('#tab-bar a').on('click', function(e){
+    e.preventDefault();
+    var nextPage = $(e.target.hash);
+    page(nextPage);
+});
+
+function page(toPage) {
+    var toPage = $(toPage),
+        fromPage = $("#pages .current");
+    if(toPage.hasClass("current") || toPage === fromPage) {
+        return;
+    };
+    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
+        fromPage.removeClass("current fade out");
+        toPage.removeClass("fade in")
+    });
+    fromPage.addClass("fade out");
+}
+
 var theScroll;
 function scroll(){
     theScroll = new iScroll('wrapper');

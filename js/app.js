33f54b1b7beb4ef5e1da6d46ab19942a58310b17
Will Parker <wprk14@gmail.com>
2013-04-16 01:00:04 +0100

    Changes to Nav

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 $('#tab-bar a').on('click', function(e){
     e.preventDefault();
     var nextPage = $(e.target.hash);
-    page(nextPage);
+    var nextLink = $('#tab-bar a[href=' + e.target.hash + ']');
+    page(nextPage, nextLink);
+    $("#pages .current").removeClass("current");
+    nextPage.addClass("current");
 });
 
-function page(toPage) {
+function page(toPage, toLink) {
     var toPage = $(toPage),
         fromPage = $("#pages .current");
+    var toLink = $(toLink),
+        fromLink = $("#tab-bar .current");
     if(toPage.hasClass("current") || toPage === fromPage) {
         return;
     };
@@ -14,6 +19,8 @@ function page(toPage) {
         fromPage.removeClass("current fade out");
         toPage.removeClass("fade in")
     });
+    fromLink.removeClass("current");
+    toLink.addClass("current");
     fromPage.addClass("fade out");
 }
 
